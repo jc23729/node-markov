51d6c435b34c3d8faa86531619aee877b77ee296
Juan Cruz <jc23729@gmail.com>
2021-02-13 17:19:36 -0500

    update makeText.js

--- makeText.js
+++ makeText.js
@@ -1,2 +1,12 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const markov = require('.markov');
+const axios = require('axios');
+const process = require('process');
+const { func } = require('prop-types');
 
+/** Make Markov machine from text and generate text from it. */
+function generateText(text) {
+    let mm = new MarkovMachine(text);
+    console.log(mm.makeText());
+}
